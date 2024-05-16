b5550843f2acf473988116f50dd4b30e8ec70a43
jakobsym <jakob.symchych@my.utsa.edu>
2024-05-16 14:08:15 -0500

    init controller for token service

--- token/internal/controller/token/controller.go
+++ token/internal/controller/token/controller.go
@@ -1,1 +1,32 @@
 package token
+
+import (
+	"context"
+	"errors"
+
+	"github.com/jakobsym/sol_service/token/internal/repository"
+	model "github.com/jakobsym/sol_service/token/pkg"
+)
+
+var ErrNotFound = errors.New("not found")
+
+type tokenRepo interface {
+	Get(ctx context.Context, address string) (*model.Token, error)
+}
+
+type Controller struct {
+	repo tokenRepo
+}
+
+func New(repo tokenRepo) *Controller {
+	return &Controller{repo: repo}
+}
+
+func (c *Controller) Get(ctx context.Context, address string) (*model.Token, error) {
+	res, err := c.repo.Get(ctx, address)
+	// check if error matches error from repository call
+	if err != nil && errors.Is(err, repository.ErrNotFound) {
+		return nil, ErrNotFound
+	}
+	return res, err
+}
