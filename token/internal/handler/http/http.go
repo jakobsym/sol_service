9426f22b6c8e4671a6ad79c35ee34c3899b733f9
jakobsym <jakob.symchych@my.utsa.edu>
2024-05-16 14:33:47 -0500

    init handler for token service, still need to implement functions

--- token/internal/handler/http/http.go
+++ token/internal/handler/http/http.go
@@ -1,1 +1,24 @@
 package httphandler
+
+import (
+	"net/http"
+
+	"github.com/jakobsym/sol_service/token/internal/controller/token"
+)
+
+type Handler struct {
+	ctrl *token.Controller
+}
+
+func New(ctrl *token.Controller) *Handler {
+	return &Handler{ctrl: ctrl}
+}
+
+func (h *Handler) GetToken(w http.ResponseWriter, r *http.Request) {
+	address := r.FormValue("address")
+	if address == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+}
