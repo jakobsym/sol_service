4fbe5b60650ab99547a2984640a9f444e585290f
jakobsym <jakob.symchych@my.utsa.edu>
2024-05-23 15:18:01 -0500

    moved files to respective locations. Implemented in-memory db for testing

---
+++ wallet/src/controller/walletController.ts
@@ -0,0 +1,3 @@
+// after we query for all tokens in wallet
+// assign only response.mint to the Wallet.tokens[]
+// creating an array of token addresses
--- wallet/src/model/wallet.ts
+++ wallet/src/model/wallet.ts
@@ -1,2 +1,14 @@
-import * as fs from 'fs';
-export interface 
+export interface Wallet {
+    walletAddress: string
+    tokens?: string[]
+}
+/*
+{
+    walletaddress: "B2MhxCCPipp85D6LFdqd94Q5X5e8gy3dsGmHDxFH5GSV",
+    tokens: [
+        '7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr',
+        '7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr',
+        '7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr'
+    ]
+}
+*/
